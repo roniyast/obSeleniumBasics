1827e2a4ea37e5b7f5e8974cf24bddc6ebc9a001
Roniya <roniyast@gmail.com>
2021-10-20 19:33:47 +0530

    20_10 Commit Selenium Basics

--- src/main/java/com/obsqura/selenium/BrowserLaunch.java
+++ src/main/java/com/obsqura/selenium/BrowserLaunch.java
@@ -7,41 +7,42 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserLaunch {
 
-    WebDriver driver;
+    static WebDriver driver;
 
-
-    public void testInitialize(String browser) {
+    public static void testInitialize(String browser) {
 
         if (browser.equalsIgnoreCase("chrome")) {
-            steps();
+            System.setProperty("webdriver.chrome.driver", "H:\\selenium\\driverfiles\\chromedriver_win32\\chromedriver.exe");
+            driver = new ChromeDriver();
         } else if (browser.equalsIgnoreCase("firefox")) {
-            steps();
+            System.setProperty("webdriver.gecko.driver", "H:\\selenium\\driverfiles\\geckodriver-v0.30.0-win32\\geckodriver.exe");
+            driver = new FirefoxDriver();
         } else if (browser.equalsIgnoreCase("edge")) {
-            steps();
-
+            System.setProperty("webdriver.edge.driver", "H:\\selenium\\driverfiles\\edgedriver_win64\\msedgedriver.exe");
+            driver = new EdgeDriver();
         }
+        steps();
     }
 
-    public void steps() {
+    public static void steps() {
+        driver.manage().deleteAllCookies();
         driver.get("http://demo.guru99.com/test/newtours/");
         driver.manage().window().maximize();
-        driver.close();
+        //driver.close();
     }
 
     public static void main(String args[]) {
 
-        BrowserLaunch launch = new BrowserLaunch();
+        BrowserLaunch.testInitialize("chrome");
+        String title = driver.getTitle();
+        System.out.println("Title :" + title);
+        String url = driver.getCurrentUrl();
+        System.out.println("URL :" + url);
+        String pageSource = driver.getPageSource();
+        System.out.println("PageSource :" + pageSource);
+        driver.close();
+        //launch.testInitialize("firefox");
+        //launch.testInitialize("edge");
 
-        System.setProperty("webdriver.chrome.driver", "H:\\selenium\\driverfiles\\chromedriver_win32\\chromedriver.exe");
-        launch.driver = new ChromeDriver();
-        launch.testInitialize("chrome");
-
-        System.setProperty("webdriver.gecko.driver", "H:\\selenium\\driverfiles\\geckodriver-v0.30.0-win32\\geckodriver.exe");
-        launch.driver = new FirefoxDriver();
-        launch.testInitialize("firefox");
-
-        System.setProperty("webdriver.edge.driver", "H:\\selenium\\driverfiles\\edgedriver_win64\\msedgedriver.exe");
-        launch.driver = new EdgeDriver();
-        launch.testInitialize("edge");
     }
 }
