89548a2e46f84b166b46b4db819888c5106567ca
Roniya <roniyast@gmail.com>
2021-11-01 12:54:54 +0530

    01_11 Commit Selenium Basics

--- src/test/java/com/obsqura/commands/Commands.java
+++ src/test/java/com/obsqura/commands/Commands.java
@@ -1,8 +1,6 @@
 package com.obsqura.commands;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,12 +9,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.*;
+
 public class Commands {
 
     WebDriver driver;
 
     public void testInitialize(String browser) {
-
         if (browser.equalsIgnoreCase("chrome")) {
             System.setProperty("webdriver.chrome.driver", "H:\\selenium\\driverfiles\\chromedriver_win32\\chromedriver.exe");
             driver = new ChromeDriver();
@@ -38,12 +37,11 @@ public class Commands {
 
     @AfterMethod
     public void tearDown() {
-        //driver.close();
+        driver.close();
     }
 
     @Test(priority = 1, enabled = false)
     public void verifyUserLogin() {
-
         driver.get("http://demowebshop.tricentis.com/");
         WebElement loginMenu = driver.findElement(By.className("ico-login"));
         loginMenu.click();
@@ -68,7 +66,7 @@ public class Commands {
         Assert.assertEquals(actualTitle, expectedTitle, "Invalid Home Page Title");
     }
 
-    @Test(priority=3,enabled=true)
+    @Test(priority = 3, enabled = false)
     public void verifyFileUpload() {
         driver.get("https://demo.guru99.com/test/upload/");
         WebElement chooseFIle = driver.findElement(By.cssSelector("input#uploadfile_0"));
@@ -79,5 +77,87 @@ public class Commands {
         submitButton.click();
     }
 
+    @Test(priority = 4, enabled = false)
+    public void verifyConfirmationAlert() {
+        driver.get("https://demo.guru99.com/test/delete_customer.php");
+        WebElement customerId = driver.findElement(By.name("cusid"));
+        customerId.sendKeys("123456");
+        WebElement submit = driver.findElement(By.name("submit"));
+        submit.click();
+
+        Alert alert = driver.switchTo().alert();
+        //alert.accept();
+        //alert.dismiss();
+        String text = alert.getText();
+        alert.accept();
+        System.out.println(text);
+    }
+
+    @Test(priority = 5, enabled = false)
+    public void verifyFramesInHTML() {
+        driver.get("https://demoqa.com/frames");
+        //driver.switchTo().frame("frame1"); //By Id
+        //driver.switchTo().frame(1);        //element no
+        WebElement frameElement = driver.findElement(By.id("frame1"));
+        driver.switchTo().frame(frameElement); //Webelement
+
+        //driver.switchTo().parentFrame(); //Methods to switch to parent frame
+        driver.switchTo().defaultContent();
+
+        WebElement header1 = driver.findElement(By.className("header-wrapper"));
+        String tag = header1.getTagName();
+        System.out.println(tag);
+
+    }
+
+    @Test(priority = 6, enabled = false)
+    public void verifyTotalNumberOfFrames() {
+        driver.get("https://demoqa.com/frames");
+        List<WebElement> webElementList = driver.findElements(By.tagName("iframe"));
+        int totalFrameNo = webElementList.size();
+        System.out.println(totalFrameNo);
+    }
+
+    //ToolsQA Alert Examples
+
+    @Test(priority = 7, enabled = true)
+    public void verifySimpleAlert() {
+        driver.get("https://demoqa.com/alerts");
+        WebElement simpleAlert = driver.findElement(By.id("alertButton"));
+        simpleAlert.click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }
+
+    @Test(priority = 8, enabled = false)
+    public void verifyFiveSecWaitAlert() throws InterruptedException {
+        driver.get("https://demoqa.com/alerts");
+        WebElement timerAlertButton = driver.findElement(By.id("timerAlertButton"));
+        timerAlertButton.click();
+        Thread.sleep(5000);
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }
+
+    @Test(priority = 9, enabled = false)
+    public void verifyToolsQAConfirmationAlert() {
+        driver.get("https://demoqa.com/alerts");
+        WebElement confirmationAlert = driver.findElement(By.id("confirmButton"));
+        confirmationAlert.click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }
+
+    @Test(priority = 10, enabled = false)
+    public void verifyPromptAlert() throws InterruptedException {
+        driver.get("https://demoqa.com/alerts");
+        WebElement promptButton = driver.findElement(By.xpath("//button[@id='promtButton']"));
+        //JavascriptExecutor js = (JavascriptExecutor)driver;
+        //js.executeScript("arguments[0].click()", promptButton);
+        promptButton.click();
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("Automation");
+        alert.accept();
+    }
 
 }
